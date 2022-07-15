64f57666fc397823adf8a020df148372b136ad55
danielware <waredanielb@gmail.com>
2022-07-15 16:07:27 -0700

    Add console changes including total price calculation.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,23 +7,57 @@ namespace Bakery.Models
   {
     public static void Main()
     {
-      Console.WriteLine("\n---Welcome to Pierre's Bakery---");
-      Console.WriteLine("---Home of the finest Breads and Pastries that money can buy---");
-      Console.WriteLine("---Our Bread is priced at $5/loaf (or buy 2 get 1 free)");
-
+      Console.WriteLine("\n--- Welcome to Pierre's Bakery ---");
+      Console.WriteLine("--- Home of the Finest Breads and Pastries that Money Can Buy ---");
+      Console.WriteLine("--- Our Bread is priced at $5/loaf (or buy 2 get 1 free!) ---");
+      Console.WriteLine("--- Pastries are currently $2 each (or 3 for $5!) ---");
       Console.WriteLine("\nWould you like to place an order? (Y/N)");
       string initialResponse = Console.ReadLine();
       if (initialResponse.ToLower() == "y" || initialResponse.ToLower() == "yes")
       {
-        Console.WriteLine("Great, how many loaves of Bread would you like?");
+        //Bread Order
+         Console.WriteLine("Great, how many loaves of Bread would you like?");
         int breadOrder = int.Parse(Console.ReadLine());
         var orderBread = new Bread(breadOrder);
-        Console.WriteLine("Fantastic. " + orderBread.BreadAmount + " loaves will run you " + orderBread.BreadCost());
+        Console.WriteLine("Fantastic. " + orderBread.BreadAmount + " loaves will run you $" + orderBread.BreadCost());
+        //Pastry Order
+        Console.WriteLine("How many Pastries would you like to add to your order?");
+        int pastryOrder = int.Parse(Console.ReadLine());
+        var orderPastry = new Pastry(pastryOrder);
+        Console.WriteLine("Ok. " + orderPastry.PastryAmount + " pastries will come to $" + orderPastry.PastryCost());
+        //Checkout
+        double orderTotal = (orderPastry.PastryCost() + orderBread.BreadCost());
+        Console.WriteLine("Alright, let me just box that up for you. I'm sure you'll enjoy our baked goods!");
+        Console.WriteLine("Your order comes to $" + orderTotal + ".");
       }
       else 
       {
-        Console.WriteLine("We hope you change your mind!");
+        Console.WriteLine("Come back soon, we hope you change your mind!");
       }
     }
+      // public static void OrderBread()
+      // {
+      //   Console.WriteLine("Great, how many loaves of Bread would you like?");
+      //   int breadOrder = int.Parse(Console.ReadLine());
+      //   var orderBread = new Bread(breadOrder);
+      //   Console.WriteLine("Fantastic. " + orderBread.BreadAmount + " loaves will run you $" + orderBread.BreadCost());
+      //   OrderPastry();
+
+      // }
+      // public static void OrderPastry()
+      // {
+      //   Console.WriteLine("Would you like to add any Pastries to your order?");
+      //   int pastryOrder = int.Parse(Console.ReadLine());
+      //   var orderPastry = new Pastry(pastryOrder);
+      //   Console.WriteLine("Ok. " + orderPastry.PastryAmount + " pastries will come to $" + orderPastry.PastryCost());
+      //   OrderTotal();
+
+      // }
+      // public static double OrderTotal()
+      // {
+      //   double orderTotal = (orderPastry.PastryCost);
+      //   Console.WriteLine("Alright, let me just box that up for you. I'm sure you'll enjoy our baked goods");
+      //   Console.WriteLine("Your order somes to $" + orderTotal);
+      // }
   }
 }
