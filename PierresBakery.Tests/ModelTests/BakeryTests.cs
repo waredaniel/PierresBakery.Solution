f16257ddbe3ea05146d41e1dd55faaa779dacc08
danielware <waredanielb@gmail.com>
2022-07-15 13:38:51 -0700

    Create and pass tests for zero item case.

--- PierresBakery.Tests/ModelTests/BakeryTests.cs
+++ PierresBakery.Tests/ModelTests/BakeryTests.cs
@@ -9,6 +9,14 @@ namespace Bakery.Tests
   public class BakeryTests
   {
     [TestMethod]
+    public void BreadCost_ZeroBread_Int()
+    {
+      int breadAmount = 0;
+      double breadCost = 0;
+      Bread testOrder = new Bread(breadAmount);
+      Assert.AreEqual(breadCost, testOrder.BreadCost());
+    }
+    [TestMethod]
     public void BreadCost_OneBread_Int()
     {
       int breadAmount = 1;
@@ -31,6 +39,22 @@ namespace Bakery.Tests
       double breadCost = 10;
       Bread testOrder = new Bread(breadAmount);
       Assert.AreEqual(breadCost, testOrder.BreadCost());
+    }
+    [TestMethod]
+    public void PastryCost_ZeroPastry_Int()
+    {
+      int pastryAmount = 0;
+      double pastryCost = 0;
+      Pastry testOrder = new Pastry(pastryAmount);
+      Assert.AreEqual(pastryCost, testOrder.PastryCost());
+    }
+    [TestMethod]
+    public void PastryCost_OnePastry_Int()
+    {
+      int pastryAmount = 0;
+      double pastryCost = 0;
+      Pastry testOrder = new Pastry(pastryAmount);
+      Assert.AreEqual(pastryCost, testOrder.PastryCost());
     }
   }
 }
